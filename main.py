da4dd95a34d18b3c260e2d2e3691b22f7991ecd1
pserel <panagiotis.sereleas@gmail.com>
2019-09-20 01:09:29 +0200

    succesfull e2e multistep prediction with multiple features, untuned not good accuracy, level etc

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint, T
 # Importing Data Set
 dataset = pd.read_csv('acn.csv')
 
-dataset_train = dataset.iloc[:len(dataset) - 25, :]
-dataset_test = dataset.iloc[len(dataset) - 25:, :]
+n_future = 25  # Number of days you want to predict into the future
+n_past = 360  # Number of past days you want to use to predict the future
+dataset_train = dataset.iloc[:len(dataset) - n_future - n_past, :]
+dataset_test = dataset.iloc[len(dataset) - n_future - n_past:, :]
  
 cols = list(dataset_train)[1:6]
 dataset_train = dataset_train[cols]
@@ -28,16 +30,15 @@ sc_predict = StandardScaler()
  
 sc_predict.fit_transform(training_set[:,0:1])
  
-# Creating a data structure with 60 timesteps and 1 output
+# Creating a data structure with 360 timesteps and 25 outputs
 X_train = []
 y_train = []
  
-n_future = 25  # Number of days you want to predict into the future
-n_past = 360  # Number of past days you want to use to predict the future
  
 for i in range(n_past, len(training_set_scaled) - n_future + 1):
     X_train.append(training_set_scaled[i - n_past:i, 0:5])
-    y_train.append(training_set_scaled[i+n_future-1:i + n_future, 0])
+    #y_train.append(training_set_scaled[i+n_future-1:i + n_future, 0])
+    y_train.append(training_set_scaled[i:i + n_future, 0])
  
 X_train, y_train = np.array(X_train), np.array(y_train)
  
@@ -54,19 +55,23 @@ regressor = Sequential()
  
 # Adding fist LSTM layer and Drop out Regularization
 regressor.add(LSTM(units=50, return_sequences=True, input_shape=(n_past, 5)))
-regressor.add(Dropout(0.3))
+regressor.add(Dropout(0.4))
  
 # Part 3 - Adding more layers
 # Adding 2nd layer with some drop out regularization
 regressor.add(LSTM(units=50, return_sequences=True))
-regressor.add(Dropout(0.3))
+regressor.add(Dropout(0.4))
 
 # Adding a third LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 50, return_sequences=True))
+regressor.add(Dropout(0.4))
+
+# Adding a fourth LSTM layer and some Dropout regularisation
 regressor.add(LSTM(units = 50))
-regressor.add(Dropout(0.3))
+regressor.add(Dropout(0.4))
 
 # Output layer
-regressor.add(Dense(units=1, activation='linear'))
+regressor.add(Dense(units=n_future))
  
 # Compiling the RNN
 regressor.compile(optimizer='adam', loss="mean_squared_error")  # Can change loss to mean-squared-error if you require.
@@ -78,36 +83,30 @@ rlr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, verbose=1)
 mcp = ModelCheckpoint(filepath='weights.h5', monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=True)
 tb = TensorBoard('logs')
  
-history = regressor.fit(X_train, y_train, shuffle=True, epochs=100, callbacks=[es, rlr, mcp, tb], validation_split=0.2, verbose=1, batch_size=64)
+history = regressor.fit(X_train, y_train, shuffle=True, epochs=100, callbacks=[es, rlr, mcp, tb], validation_split=0.2, verbose=1, batch_size=128)
  
  
 # Predicting the future.
-#--------------------------------------------------------
-# The last date for our training set is 05-Aug-2019.
-# Lets now try predicting the stocks for the dates in the test set.
- 
-# The dates on our test set are:
-# 6,7,8,9,12,13,14,15,16,19,20,21,22,23,26,27,28,29,30-Aug-2019 and 3,4,5,6,9,10-Sep-2019
- 
-# Now, the latest we can predict into our test set is to the 19th since the last date on training is 30-Dec-2016. 
-# 20 days into the future from the latest day in our training set is 19-Dec-2016. Right?
-# Notice that we dont have some days in our test set, what we can do is to take the last 20 samples from the training set. 
-# (Remember the last sample of our training set will predict the 19th of Jan 2017, the second last will predict the 18th, etc)
- 
  
 
-y_true = np.array(dataset_test['open'])
-#Trim the test set to first 12 entries (till the 19th)
-y_true = y_true[0:12]
-predictions = regressor.predict(X_train[-25:])
+y_true = np.array(dataset_test.open[len(dataset_test) - n_future:])
  
- 
-# We skip the 31-Dec, 1-Jan,2-Jan, etc to compare with the test_set
-predictions_to_compare = predictions[[3,4,5,6,9,10,11,12,13,17,18,19]]
-y_pred = sc_predict.inverse_transform(predictions_to_compare)
- 
- 
- 
+test_set = dataset_test.iloc[:len(dataset_test) - n_future,1:6].values
+test_set_scaled = sc.transform(test_set)
+
+# Creating a data structure with 360 timesteps and 25 outputs
+X_test = []
+for i in range(0, n_past):
+    X_test.append(test_set_scaled[i, 0:5])
+X_test = np.array(X_test)
+
+# Reshaping
+X_test = np.reshape(X_test, (1, X_test.shape[0], X_test.shape[1]))
+
+predictions = regressor.predict(X_test)
+
+y_pred = sc_predict.inverse_transform(predictions).reshape(25,1)
+
 hfm, = plt.plot(y_pred, 'r', label='predicted_stock_price')
 hfm2, = plt.plot(y_true,'b', label = 'actual_stock_price')
  
@@ -120,9 +119,9 @@ plt.show()
 plt.close()
  
  
- 
-hfm, = plt.plot(sc_predict.inverse_transform(y_train), 'r', label='actual_training_stock_price')
-hfm2, = plt.plot(sc_predict.inverse_transform(regressor.predict(X_train)),'b', label = 'predicted_training_stock_price')
+train_predict = np.reshape(X_train[len(X_train)-1], (1, 360, 5))
+hfm, = plt.plot(sc_predict.inverse_transform(y_train[len(y_train)-1,:]), 'r', label='actual_training_stock_price')
+hfm2, = plt.plot(sc_predict.inverse_transform(regressor.predict(train_predict)).reshape(25,1),'b', label = 'predicted_training_stock_price')
  
 plt.legend(handles=[hfm,hfm2])
 plt.title('Predictions vs Actual Price')
